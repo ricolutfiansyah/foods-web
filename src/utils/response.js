986e291849f9416d4e49ae2fda6c5922626fb2d4
Rico <rahmandarico3@gmail.com>
2026-04-28 13:52:56 +0700

    feat: add utils (asyncHandler, AppError, response, jwt, pagination)

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,2 +1,29 @@
-// TODO: Implement response helper
-export default {};
+export const sendResponse = (res, statusCode, message, data = null, meta = null) => {
+  const response = {
+    success: true,
+    message,
+  };
+
+  if (data !== null && data !== undefined) {
+    response.data = data;
+  }
+
+  if (meta !== null && meta !== undefined) {
+    response.meta = meta;
+  }
+
+  return res.status(statusCode).json(response);
+};
+
+export const sendError = (res, statusCode, message, errors = null) => {
+  const response = {
+    success: false,
+    message,
+  };
+
+  if (errors) {
+    response.errors = errors;
+  }
+
+  return res.status(statusCode).json(response);
+};
