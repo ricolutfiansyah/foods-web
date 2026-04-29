4a06e7ea96de31c471f516b50c6c3985445b1235
Rico <rahmandarico3@gmail.com>
2026-04-29 18:42:04 +0700

    feat: add rate limiting

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,2 +1,26 @@
-// TODO: Implement rate limiter
-export default {};
+import rateLimit from 'express-rate-limit';
+
+const limitHandler = {
+    windowMs: 15 * 60 * 1000,
+    message: {
+        success: false,
+        message: "Too many requests, please try again later."
+    },
+    statusCode: 429,
+    skip: () => process.env.NODE_ENV === 'development'
+};
+
+export const globalLimiter = rateLimit({
+    ...limitHandler,
+    limit: 100
+});
+
+export const authLimiter = rateLimit({
+    ...limitHandler,
+    limit: 10
+});
+
+export const strictLimiter = rateLimit({
+    ...limitHandler,
+    limit: 30
+});
