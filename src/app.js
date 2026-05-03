39245ac00610e257211b42b630c10354c2f8fe54
Rico <rahmandarico3@gmail.com>
2026-05-03 17:37:25 +0700

    Cors update

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(globalLimiter);
