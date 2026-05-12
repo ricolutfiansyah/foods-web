57704d4e2d043f0e3bc1e286e4e4c06c72ffc9b1
Rico <rahmandarico3@gmail.com>
2026-05-12 09:48:21 +0700

    feat/validator-management

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -6,6 +6,6 @@ export const registerSchema = z.object({
 });
 
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email format"),
+  email: z.string().min(1, "Email is required").email("Invalid email format"),
   password: z.string().min(1, "Password is required"),
 });
